6517e69cb5675574411dda760198bc9bc66164db
Alexs <ec22908@qmul.ac.uk>
2024-04-06 22:13:36 +0100

    qol

--- assignskilldev.php
+++ assignskilldev.php
@@ -2,11 +2,11 @@
 <?php
 session_start();
 require 'db.php'; // Ensure you have included the database connection
-if (!isset($_SESSION['email'])) {
-    // If the user is not logged in, redirect to the login page.
-    header('Location: index.php');
-    exit();
-}
+// if (!isset($_SESSION['email'])) {
+//     // If the user is not logged in, redirect to the login page.
+//     header('Location: index.php');
+//     exit();
+// }
 // Check if the form data is set
 if (isset($_POST['Group_User'], $_POST['Task'], $_POST['CompletionDate'])) {
     $groupUser = $_POST['Group_User']; // Updated field name
@@ -20,7 +20,7 @@ if (isset($_POST['Group_User'], $_POST['Task'], $_POST['CompletionDate'])) {
     if ($stmt->execute()) {
         // Update was successful
         $_SESSION['email'] = $newEmail; // Update the email in the session if it was changed
-        header('Location: updateinfo.php?success=1'); // Redirect with a success message
+        header('Location: trainerManagement.html'); // Redirect with a success message
     } else {
         echo "Error: " . $stmt->error;
     }
